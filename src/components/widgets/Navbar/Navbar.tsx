804338dc38fcceebb08d0a483491da145f23f6c8
Eugene Gatsak <greenglaz@gmail.com>
2023-03-16 15:00:30 +0400

    reat: add withrouter hoc

--- src/components/widgets/Navbar/Navbar.tsx
+++ src/components/widgets/Navbar/Navbar.tsx
@@ -1,20 +1,34 @@
 import { Component } from "react";
 import AppLink from "../../shared/ui/AppLink/AppLink";
+import { withRouter, WithRouterProps } from "../../router/provider/withRouter";
 import { RoutePath } from "../../router/";
 import styles from "./Navbar.module.css";
 
-export default class Navbar extends Component {
+const AppRoutesUi: Record<string, string> = {
+  "/": "MAIN PAGE",
+  "/about": "ABOUT PAGE",
+};
+
+class Navbar extends Component<WithRouterProps> {
+  constructor(props: WithRouterProps) {
+    super(props);
+  }
   render() {
     return (
       <nav className={styles.navbar}>
-        <AppLink to={RoutePath.main} className={styles.item} data-testid="main-link">
-          Main
-        </AppLink>
+        <div>{AppRoutesUi[this.props.location.pathname] ?? "NOT FOUND PAGE"}</div>
+        <div className={styles["nav-items"]}>
+          <AppLink to={RoutePath.main} className={styles.item} data-testid="main-link">
+            Main
+          </AppLink>
 
-        <AppLink to={RoutePath.about} className={styles.item} data-testid="about-link">
-          About
-        </AppLink>
+          <AppLink to={RoutePath.about} className={styles.item} data-testid="about-link">
+            About
+          </AppLink>
+        </div>
       </nav>
     );
   }
 }
+
+export default withRouter(Navbar);
